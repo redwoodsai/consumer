dea43418be721848938aa28d9375767ad38942f2
ananta.mohanty <ananta.mohanty@globallogic.com>
2023-12-27 20:38:03 +0530

    Consumer code change

---
+++ src/test/java/com/redwoods/consumer/consumerservice/ConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redwoods.consumer.consumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
